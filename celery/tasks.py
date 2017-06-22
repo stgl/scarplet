adaac024ffe1c9097a866917f9e12a42b1b57d57
rmsare <robertmsare@gmail.com>
2017-06-22 15:58:25 -0700

    Remove testing functions from tasks file

--- celery/tasks.py
+++ celery/tasks.py
@@ -14,9 +14,9 @@ from timeit import default_timer as timer
 app = Celery('scarplet-testing', broker='sqs://')
 app.config_from_object('celeryconfig')
 
-data = dem.DEMGrid('carrizo.tif')
+#data = dem.DEMGrid('carrizo.tif')
 #data = dem.DEMGrid('synthetic_kt10_1000.tif')
-#data = wt.generate_synthetic_scarp(1, 0, 10, 200, 200)
+data = wt.generate_synthetic_scarp(1, 0, 10, 200, 200)
 
 @app.task(ignore_result=False)
 def long_function(t, n=100):
@@ -40,31 +40,4 @@ def load_data(filename):
 def match_template(d, age, alpha):
     amp, snr = scarplet.calculate_amplitude(data, wt.Scarp, d, age, alpha)
     return TemplateFit(d, age, alpha, amp, snr)
-
-def calculate_best_fit_parameters(d=100):
-
-    max_age = 3 
-    age_step = 0.5
-    num_ages = max_age/age_step
-    ang_step = 2
-    num_angles = 180/ang_step + 1
-    ages = 10**np.linspace(0, max_age, num=num_ages)
-    angles = np.linspace(-np.pi/2, np.pi/2, num=num_angles)
-    
-    start = timer()
-    job = group([match_template.s(d, age, alpha) for age in ages for alpha in angles])
-    res = job.apply_async()
-
-    results = scarplet.compare_fits(data, res.join())
-
-    res.forget()
-    t_tot = timer() - start
-
-    print("Template matching complete")
-    print("-"*30)
-    #print("Time for async fits:\t " + "{:.1f}".format(t_fits) + " s")
-    #print("Time for comparison:\t " + "{:.1f}".format(t_comp) + " s")
-    print("Total time:\t\t " + "{:.1f}".format(t_tot) + " s")
-
-    return results
 
