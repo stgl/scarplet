15f36bb92ab7ad4d1886977996383732ad791a20
Ubuntu <ubuntu@ip-172-31-5-68.us-west-2.compute.internal>
2017-06-22 21:08:31 +0000

    Parameter update

--- celery/run_test.py
+++ celery/run_test.py
@@ -20,12 +20,13 @@ import matplotlib.pyplot as plt
 data = generate_synthetic_scarp(1, 0, 10, 200, 200)
 
 d = 100
-max_age = 10**3.5
-age_step = 1
+max_age = 3 
+age_step = 0.5
 num_ages = max_age/age_step
-num_angles = 180
-#ages = np.linspace(0, max_age, num=num_ages)
-#angles = np.linspace(-np.pi/2, np.pi/2, num=num_angles)
+ang_step = 2
+num_angles = 180/ang_step + 1
+ages = 10**np.linspace(0, max_age, num=num_ages)
+angles = np.linspace(-np.pi/2, np.pi/2, num=num_angles)
 
 res = tasks.calculate_best_fit_parameters()
 
