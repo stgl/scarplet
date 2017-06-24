fd8ead4f40703ba86efe201c0d2434d9f209ae73
rmsare <robertmsare@gmail.com>
2017-06-24 15:40:21 -0700

    Spin up workers based on size of search space

--- scarplet/scarplet-aws.py
+++ scarplet/scarplet-aws.py
@@ -1,10 +1,21 @@
 import dem, scarplet
 
 import tasks
-from utils import save_data_to_s3
+form utils import pairs
+
+from s3utils import save_data_to_s3
+from manage_workers import start_multiple_workers, stop_all_workers
 
 import boto
 from celery import *
+
+connection = boto.connect_ec2()
+
+dage = 0.5
+x = np.linspace(0, 3.5, 3.5/dage)
+age_breaks = pairs(x)
+n = len([x for x in age_breaks])
+start_multiple_workers(connection, n)
 
 g = group([tasks.match_chunk.s(a, b) for a, b in age_breaks])
 r = g.apply_async()
@@ -16,5 +27,5 @@ r.forget()
 
 best_results = compare_fits_from_s3()
 
-save_data_to_s3(best_results, bucket_name='scarp-results')
-
+save_data_to_s3(best_results, results_filename, bucket_name='scarp-results')
+stop_all_workers(connection)
