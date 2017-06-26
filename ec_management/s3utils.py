59226a065c8b4d9a199464cf5008208f506f60df
rmsare <robertmsare@gmail.com>
2017-06-26 11:43:10 -0700

    Add import, misc variable name change

--- ec_management/s3utils.py
+++ ec_management/s3utils.py
@@ -4,6 +4,7 @@ Utilities for S3 data transfer
 
 import boto
 import datetime
+import numpy as np
 
 def load_data_from_s3(filename, bucket_name='scarp-tmp'):
     connection = boto.connect_s3()
@@ -18,13 +19,13 @@ def download_data_from_s3(filename, bucket_name='scarp-tmp'):
     key = bucket.new_key(filename)
     key.get_contents_to_filename(filename)
 
-def save_data_to_s3(results, filename=None, bucket_name='scarp-tmp'):
+def save_data_to_s3(data, filename=None, bucket_name='scarp-tmp'):
     connection = boto.connect_s3()
     bucket = connection.get_bucket(bucket_name, validate=False)
     if not filename:
         d = datetime.datetime.now()
         filename = 'tmp_' + d.isoformat() + '.npy'
     key = bucket.new_key(filename)
-    np.save(filename, results)
+    np.save(filename, data)
     key.set_contents_from_filename(filename)
 
