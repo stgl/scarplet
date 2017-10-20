5d665d89809bf442e6dac368c6c07a6d387b4a00
rmsare <robertmsare@gmail.com>
2017-10-20 15:49:26 -0700

    Remove celery task management -- this functionality is now in scarp-reduce

--- ec_management/download_current_data.py
+++
@@ -1,5 +0,0 @@
-from s3utils import download_data_from_s3
-from config import CURRENT_DATA_FILENAME
-
-if __name__ == "__main__":
-    download_data_from_s3(CURRENT_DATA_FILENAME, bucket_name="scarp-data")
