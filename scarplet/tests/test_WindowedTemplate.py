e6b36d308ebe98172a32d891cc7d9d4ed9b5996d
rmsare <robertmsare@gmail.com>
2018-10-13 07:48:39 -0700

    Add template test cases

--- scarplet/tests/test_WindowedTemplate.py
+++ scarplet/tests/test_WindowedTemplate.py
@@ -2,17 +2,60 @@ import unittest
 
 import numpy as np
 from osgeo import gdal, osr, ogr
-from scarplet import dem
 
 from scipy.special import erf
+
+import scarplet as sl
+
+from scarplet.WindowedTemplate import Scarp, Channel
 
 DEFAULT_EPSG = 32610 # UTM 10N
 
 
-class WindowedTemplateTestCase(unittest.TestCase):
-    pass    
-
 
 class ScarpTestCase(unittest.TestCase):
-    pass
 
+
+    def setUp(self):
+    
+        self.obj = Scarp(100, 10, 0, 100, 100, 1)
+
+    def test_template(self):
+
+        true = np.load('results/scarp_template.npy')
+        test = self.obj.template()
+
+        self.assertTrue(np.allclose(test, true), "Scarp template function is \
+                        incorrect")
+
+    def test_template_numexpr(self):
+
+        true = np.load('results/scarp_template.npy')
+        test = self.obj.template_numexpr()
+
+        self.assertTrue(np.allclose(test, true), "Scarp template function is \
+                        incorrect")
+
+
+class ChannelTestCase(unittest.TestCase):
+
+
+    def setUp(self):
+    
+        self.obj = Channel(100, 0.1, 0, 100, 100, 1)
+
+    def test_template(self):
+
+        true = np.load('results/channel_template.npy')
+        test = self.obj.template()
+
+        self.assertTrue(np.allclose(test, true), "Channel template function is \
+                        incorrect")
+
+    def test_template_numexpr(self):
+
+        true = np.load('results/channel_template.npy')
+        test = self.obj.template_numexpr()
+
+        self.assertTrue(np.allclose(test, true), "Channel template function is \
+                        incorrect")
