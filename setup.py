db495a8f17cdad35a3196ea071263c7001c7f48a
rmsare <robertmsare@gmail.com>
2018-11-20 09:50:00 -0800

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="scarplet",
-    version="0.1.2",
+    version="0.1.3",
     author=["Robert Sare", "George Hilley"],
     author_email="rmsare@stanford.edu",
     description="A Python package for topographic template matching",
